e93462cada3868382e356726cdf2b22dbc4a8ced
PATILPRASHANTR <98041934+PATILPRASHANTR@users.noreply.github.com>
2022-05-18 09:22:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Preview from './Preview';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Preview/>
+    
+      
+    </div>
+  );
+}
+
+export default App;
